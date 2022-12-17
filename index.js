b5daccbfa8f7b3cfbfa440be4286107c4035ff14
MarsPrins <prinsmars08@gmail.com>
2022-12-17 16:59:59 +0100

    second commit

--- index.js
+++ index.js
@@ -1,31 +1,6 @@
-// function reveal() {
-//     const reveal = document.querySelectorAll(".reveal");
-//     const firstImage = document.querySelectorAll(".firstImage")
-//     const firstText = document.querySelectorAll("firstText")
-
-//     for (var i = 0; i < reveal.length; i++) {
-//       var windowHeight = window.innerHeight;
-//       var elementTop = reveal[i].getBoundingClientRect().top;
-//       var elementVisible = 200;
-  
-//       if (elementTop < windowHeight - elementVisible) {
-//         aboutMe[i].classList.add("active");
-//       } else {
-//         aboutMe[i].classList.remove("active");
-//       }
-
-//       // var FMelementVisible = 350;
-//       // if (elementTop < windowHeight - FMelementVisible) {
-//       //   firstImage[i].classList.add("active");
-//       //   firstText[i].classList.add("active")
-//       // } else {
-//       //   firstImage[i].classList.remove("active");
-//       //   firstText[i].classList.remove("active")
-//       //   }
-//     }
-//   }
-  
-//   window.addEventListener("scroll", reveal);
+function delay(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
 
 
 function myFunction(x) {
@@ -44,6 +19,8 @@ var firstTitle = document.getElementsByClassName("firstPartTitle")
 var firstText = document.getElementsByClassName("firstPartText")
 var firstFiverrTop = document.getElementsByClassName("firstPartFiverrTop")
 var firstFiverrBottom = document.getElementsByClassName("firstPartFiverrBottom")
+
+doSomething
 
 function doSomething() {
   // console.log(lastKnownScrollPosition)
@@ -92,20 +69,70 @@ document.addEventListener('scroll', function(e) {
 });
 
 var fiverrTopH2Contain = document.getElementsByClassName("fiverrTopH2Contain");
-var fiverrTopH2 = document.getElementsByClassName("fiverrTopH2");
+var fiverrH2 = document.getElementsByClassName("fiverrTopH2");
 var fiverrTopP = document.getElementsByClassName("fiverrTopP");
-var briefcase = document.getElementsByClassName("fa-briefcase");
+var paperPlane = document.getElementsByClassName("fa-paper-plane");
+var me = document.getElementsByClassName("fiverrTopMe")
 
 setInterval(() => {
   if (document.querySelector(".firstPartFiverrTop:hover") != null) {
       fiverrTopH2Contain[0].classList.add("hovered");
-      fiverrTopH2[0].classList.add("hovered");
-      fiverrTopP[0].classList.add("hovered")
-      briefcase[0].classList.add("hovered")
+      fiverrH2[0].classList.add("hovered");
+      fiverrTopP[0].classList.add("hovered");
+      paperPlane[0].classList.add("hovered");
+      me[0].classList.add('hovered')
   } else {
     fiverrTopH2Contain[0].classList.remove("hovered");
-    fiverrTopH2[0].classList.remove("hovered");
-    fiverrTopP[0].classList.remove("hovered")
-    briefcase[0].classList.remove("hovered")
+    fiverrH2[0].classList.remove("hovered");
+    fiverrTopP[0].classList.remove("hovered");
+    paperPlane[0].classList.remove("hovered");
+    me[0].classList.remove('hovered')
   }
 }, 10);
+
+
+function contact() {
+  document.getElementById('firstPart').scrollIntoView();
+}
+
+function fiverr() {
+  window.open('https://www.fiverr.com/share/EormkD', '_blank')
+}
+
+
+var dataReload = document.querySelectorAll("[data-reload]");
+
+var language = {
+  ned: {
+    landNav1: 'Wat Ik Voor U Zal Doen',
+    landNav2: 'Mijn Werk',
+    landNav3: 'Kom Bij Het Team!',
+    fiverrTopH2: ' Contact <br/><var>Bericht <br/>Benader <br/>Werk Met </var>',
+    fiverrTopMe: 'Mij!',
+    fiverrTopParagraph: "<strong>Laten we in gesprek gaan!</strong> Vertel wat u van plan bent en dan zullen we praten over <strong>wat ik voor u kan beteken!</strong> Weet u nog niet zeker wat u gedaan wil hebben?<strong> Ik help u!</strong>",
+    fiverrBottomPara: 'Alternatief, Kunt u ons berichten op <a href="https://www.fiverr.com/share/EormkD" target="_blank">Fiverr</a>',
+  }
+};
+
+if (window.location.hash) {
+  if (window.location.hash === "#ned") {
+    landNav1.textContent = language.ned.landNav1;
+    landNav2.textContent = language.ned.landNav2;
+    landNav3.textContent = language.ned.landNav3;
+    firstTitle1.textContent = language.ned.landNav1 + ";";
+    firstTitle2.textContent = language.ned.landNav1 + ";";
+    fiverrTopH2.innerHTML = language.ned.fiverrTopH2;
+    fiverrTopMe.textContent = language.ned.fiverrTopMe;
+    fiverrTopParagraph.innerHTML = language.ned.fiverrTopParagraph;
+    fiverrTopParagraph.innerHTML = language.ned.fiverrTopParagraph;
+    fiverrBottomPara.innerHTML = language.ned.fiverrBottomPara;
+  }
+}
+
+
+
+for (i = 0; i <= dataReload.length; i++) {
+  dataReload[i].onclick = function() {
+    delay(200).then(() => location.reload(true))
+  }
+}
